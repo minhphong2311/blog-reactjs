3639ad1c3b95cc3099df568b2f64337fef363517
minhphong2311 <minhphong2311@gmail.com>
2023-10-07 10:42:58 +0700

    Users table pagination

---
+++ src/components/common/DataTable.js
@@ -0,0 +1,107 @@
+import React from 'react'
+
+const DataTable = (props) => {
+    const { name, data, columns, currentPage, numOfPage, onPageChange, onChangeItemsPerPage } = props;
+
+    const renderHeaders = () => {
+        return columns.map((col, index) => <th key={index}>{col.name}</th>)
+    }
+
+    const renderData = () =>{
+        return (
+            data.map((item, index) => (
+                <tr key={index}>
+                    {
+                        columns.map((col, ind) => <td key={ind}>{col.element(item)}</td>)
+                    }
+                </tr>
+            ))
+        )
+    }
+
+    const renderPagination = () => {
+        const pagination = [];
+        const nextPage = currentPage + 1 > numOfPage ? null : currentPage + 1;
+        const prevPage = currentPage - 1 ? null : currentPage - 1;
+
+        pagination.push(
+            <li key="prev" className={prevPage ? "page-item" : "page-item disabled"}>
+                <button className='page-link' onClick={() => onPageChange(prevPage)}>&laquo;</button>
+            </li>
+        )
+
+        for(let i = 1; i <= numOfPage; i++) {
+            pagination.push(
+                <li key={i} className={currentPage === i ? "page-item active" : "page-item"}>
+                    <button className='page-link' onClick={() => onPageChange(i)}>{i}</button>
+                </li>
+            )
+        }
+
+        pagination.push(
+            <li key="next" className={nextPage ? "page-item" : "page-item disabled"}>
+                <button className='page-link' onClick={() => onPageChange(nextPage)}>&raquo;</button>
+            </li>
+        )
+
+        return pagination;
+    }
+
+    const onChangeOption = (event) => {
+        const target = event.target;
+        console.log(target.value);
+        onChangeItemsPerPage(target.value)
+    }
+
+    return (
+        <div className="card mb-4">
+            <div className="card-header">
+                <i className="fas fa-table me-1"></i>
+                {name}
+            </div>
+            <div className="card-body">
+                <div className='row mb-3'>
+                    <div className='col-sm-12 col-md-6'>
+                        <label className='d-inline-flex'>Show
+                        <select name='' className='form-select form-select-sm ms-1 me-1' onChange={onChangeOption}>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="10">10</option>
+                        </select> entries
+                        </label>
+                    </div>
+                    <div className='col-sm-12 col-md-6'>
+                        <label className='d-inline-flex float-end'>Search: 
+                            <input type='search' className='form-control form-control-sm ms-1' placeholder='Email or Name' />
+                        </label>
+                    </div>
+                </div>
+                <table class="table table-bordered table-striped" width='100%'>
+                    <thead>
+                        <tr>
+                            {renderHeaders()}
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            {renderHeaders()}
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        {renderData()}
+                    </tbody>
+                </table>
+                {numOfPage > 1 && (
+                    <ul className="pagination justify-content-center">
+                        {renderPagination()}
+                    </ul>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default DataTable
---
+++ src/components/user/UserList.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react'
+import requestApi from '../../helpers/api'
+import { useDispatch } from 'react-redux'
+import * as actions from '../../redux/actions'
+import DataTable from '../common/DataTable'
+
+const UserList = () => {
+    const dispatch = useDispatch()
+    const [users, setUsers] = useState([])
+    const [numOfPage, setNumOfPage] = useState(1)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [itemsPerPage, setItemsPerPage] = useState(1)
+
+    const columns = [
+        {
+            name: "ID",
+            element: row => row.id
+        },
+        {
+            name: "First Name",
+            element: row => row.first_name
+        },
+        {
+            name: "Last Name",
+            element: row => row.last_name
+        },
+        {
+            name: "Email",
+            element: row => row.email
+        },
+        {
+            name: "Created at",
+            element: row => row.created_at
+        },
+        {
+            name: "Updated at",
+            element: row => row.updated_at
+        },
+        {
+            name: "Actions",
+            element: row => (
+                <>
+                    <button type="button" class="btn btn-primary btn-sm me-1"><i className='fa fa-pencil'></i> Edit</button>
+                    <button type="button" class="btn btn-danger btn-sm me-1"><i className='fa fa-trash'></i> Delete</button>
+                </>
+            )
+        },
+    ]
+
+    useEffect(() => {
+        dispatch(actions.controlLoading(true))
+        let query = `?items_per_page=${itemsPerPage}&page=${currentPage}`
+        requestApi(`/users${query}`, 'GET', []).then(response => {
+            console.log('response =>', response)
+            setUsers(response.data.data)
+            setNumOfPage(response.data.lastPage)
+            dispatch(actions.controlLoading(false))
+        }).catch(err => {
+            console.log(err)
+            dispatch(actions.controlLoading(false))
+        })
+    }, [currentPage, itemsPerPage])
+
+    return (
+        <div id='layoutSidenav_content'>
+            <main>
+                <div className="container-fluid px-4">
+                    <h1 className="mt-4">Tables</h1>
+                    <ol className="breadcrumb mb-4">
+                        <li className="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                        <li className="breadcrumb-item active">Tables</li>
+                    </ol>
+                    <DataTable 
+                        name="List Users" 
+                        data={users} 
+                        columns={columns}
+                        numOfPage={numOfPage}
+                        currentPage={currentPage}
+                        onPageChange={setCurrentPage}
+                        onChangeItemsPerPage={setItemsPerPage}
+                    />
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default UserList
