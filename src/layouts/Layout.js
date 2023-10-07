1f9e3c75d31b54cd422d4169f7d2c7c012d07c50
minhphong2311 <minhphong2311@gmail.com>
2023-10-07 08:22:30 +0700

    loading with react context

---
+++ src/layouts/Layout.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import { Outlet } from 'react-router-dom'
+import { ScaleLoader } from 'react-spinners'
+import { LoadingContext } from '../contexts/LoadingContext'
+
+const Layout = () => {
+    const {isLoading} = useContext(LoadingContext)
+    return (
+        <div>
+            <ScaleLoader loading={isLoading} cssOverride={{
+                position: 'absolute',
+                top: '0',
+                left: '0',
+                textAlign: 'center',
+                right: '0',
+                bottom: '0',
+                backgroundColor: 'rgb(0 0 0 / 30%)',
+                zIndex: '999'
+            }}
+            color='#36d7b7' />
+            <Outlet />
+        </div>
+    )
+}
+
+export default Layout
